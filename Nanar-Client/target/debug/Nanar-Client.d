498de8df7cfb4410212d76a5768c48cfb1c5b7f6
Muhammad Muazen <muhammadmuazen1@gmail.com>
2025-04-26 20:53:46 -1200

    adding the init files

---
+++ Nanar-Client/target/debug/Nanar-Client.d
@@ -0,0 +1,1 @@
+D:\personal\projects\NanarC2\Nanar-Client\target\debug\Nanar-Client.exe: D:\personal\projects\NanarC2\Nanar-Client\src\main.rs
---
+++ Nanar-Client/target/debug/deps/Nanar_Client.d
@@ -0,0 +1,5 @@
+D:\personal\projects\NanarC2\Nanar-Client\target\debug\deps\Nanar_Client.exe: src\main.rs
+
+D:\personal\projects\NanarC2\Nanar-Client\target\debug\deps\Nanar_Client.d: src\main.rs
+
+src\main.rs:
