5442f8ac83d665acfb216959bca076132c8f292a
Muhammad Muazen <muhammadmuazen1@gmail.com>
2025-05-08 01:49:35 -1200

    modifying the processes listing function

--- Nanar-Client/src/ps_functions.rs
+++ Nanar-Client/src/ps_functions.rs
@@ -10,23 +10,41 @@ pub fn process_list() -> String {
 
     let task_list: tasklist::Tasklist = tasklist::Tasklist::new().unwrap();
     let mut task_list_buffer: String = String::new();
-    
-    tasklist::enable_debug_priv();
 
     for task in task_list {
 
         if task.get_user().is_err() {
             task_list_buffer.push_str(
                 format!(
-                    "Process Id:{}\nProcess Name:{}\nProcess User: {}\nProcess Info: {}\n-------------------------\n", 
-                    task.get_pid(), task.get_pname(), "[-] Error: Access is denied",
-                    "[-] Error: Access is denied").as_str());
+                    "Process Id --> {}\nProcess Name: {}\nProcess User --> {}\nProcess Executable File --> {}\n\
+                    Arch --> Is_WoW: {}, Process_Arch: {}, Native_Arch: {}\n\
+                    Memory Usage --> {} MB\n\
+                    \n---------------------------------------------------------------------------\n", 
+                    task.get_pid(), 
+                    task.get_pname(), 
+                    task.get_user().unwrap_or("[-] Error: Access is denied".to_string()),
+                    task.get_path().unwrap_or("[-] Error: Access is denied".to_string()).as_str(), 
+                    task.get_architecture_info().unwrap_or((false, "UNKNOWN", "UNKNOWN")).0,
+                    task.get_architecture_info().unwrap_or((false, "UNKNOWN", "UNKNOWN")).1,
+                    task.get_architecture_info().unwrap_or((false, "UNKNOWN", "UNKNOWN")).2,
+                    task.get_memory_info().get_total_memory_usage() / (1024 * 1024))
+                    .as_str());
         } else {
             task_list_buffer.push_str(
                 format!(
-                    "Process Id: {}\nProcess Name: {}\nProcess User: {}\nProcess Info: {:?}\n-------------------------\n", 
-                    task.get_pid(), task.get_pname(), task.get_user().unwrap().as_str(),
-                    task.get_file_info()).as_str());
+                    "Process Id --> {}\nProcess Name --> {}\nProcess User --> {}\nProcess Executable File --> {}\n\
+                    Arch --> Is_WoW: {}, Process_Arch: {}, Native_Arch: {}\n\
+                    Memory Usage --> {} MB\n\
+                    \n---------------------------------------------------------------------------\n", 
+                    task.get_pid(), 
+                    task.get_pname(), 
+                    task.get_user().unwrap().as_str(),
+                    task.get_path().unwrap().as_str(), 
+                    task.get_architecture_info().unwrap_or((false, "UNKNOWN", "UNKNOWN")).0,
+                    task.get_architecture_info().unwrap_or((false, "UNKNOWN", "UNKNOWN")).1,
+                    task.get_architecture_info().unwrap_or((false, "UNKNOWN", "UNKNOWN")).2,
+                    task.get_memory_info().get_total_memory_usage() / (1024 * 1024))
+                    .as_str());
         }
     }
 
