61c104360a90639ec81e5ffb7d6e267fbbd9e225
Muhammad Muazen <muhammadmuazen1@gmail.com>
2025-05-10 17:52:09 -1200

    testing more with the connection. waiting for the server writing fellows to do something so I can funking start writing some client connection code!!!!

--- Nanar-Client/src/connection_handler.rs
+++ Nanar-Client/src/connection_handler.rs
@@ -2,14 +2,27 @@ use std::io::Write;
 
 pub fn init_conn_with_server(addr_port: &str, init_conn_pass: &str) -> std::io::Result<()> {
 
-    let mut stream = std::net::TcpStream::connect(addr_port)
+    let mut time_before_heartbeat: i16 = 3000;
+
+    let mut stream: std::net::TcpStream = std::net::TcpStream::connect(addr_port)
         .expect("[-] Error: Could not connect to the server");
 
-    
-    match stream.write_all(init_conn_pass.as_bytes()) {
-        Ok(_) => println!("Write successful"),
-        Err(e) => eprintln!("Write failed: {}", e),
+    loop {
+        match stream.write_all(init_conn_pass.as_bytes()) {
+            Ok(_) => println!("Write successful"),
+            Err(e) => eprintln!("Write failed: {}", e),
+
+        }
+
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+        time_before_heartbeat -= 1000;
+
+        if time_before_heartbeat == 0 {
+            
+            break;
+        }
     }
+
 
 
     stream.shutdown(std::net::Shutdown::Both).expect("shutdown call failed");
--- Nanar-Client/src/main.rs
+++ Nanar-Client/src/main.rs
@@ -3,5 +3,8 @@ use nanar_client::*;
 
 fn main() {
     
-    println!("{}", ps_functions::process_list());
+    let addr_port: &str = "127.0.0.1:9999";
+    let init_conn_pass: &str = "what is my name\n";
+
+    let _ = connection_handler::init_conn_with_server(addr_port, init_conn_pass);
 }
