e329a86cd0982cf610971f6e02f33543a768e98e
Muhammad Muazen <muhammadmuazen1@gmail.com>
2025-05-10 17:54:30 -1200

    updating cargo in nanar_client

--- Nanar-Client/target/release/deps/nanar_client-e5c3293ef9a493c9.d
+++ Nanar-Client/target/release/deps/nanar_client-e5c3293ef9a493c9.d
@@ -1,11 +1,11 @@
-D:\personal\projects\NanarC2\Nanar-Client\target\release\deps\libnanar_client-e5c3293ef9a493c9.rmeta: src\lib.rs src\fs_functions.rs src\fs_functions\fs_helper.rs src\connection_handler.rs src\ps_mgmt.rs
+D:\personal\projects\NanarC2\Nanar-Client\target\release\deps\libnanar_client-e5c3293ef9a493c9.rmeta: src\lib.rs src\fs_functions.rs src\fs_functions\fs_helper.rs src\connection_handler.rs src\ps_functions.rs
 
-D:\personal\projects\NanarC2\Nanar-Client\target\release\deps\libnanar_client-e5c3293ef9a493c9.rlib: src\lib.rs src\fs_functions.rs src\fs_functions\fs_helper.rs src\connection_handler.rs src\ps_mgmt.rs
+D:\personal\projects\NanarC2\Nanar-Client\target\release\deps\libnanar_client-e5c3293ef9a493c9.rlib: src\lib.rs src\fs_functions.rs src\fs_functions\fs_helper.rs src\connection_handler.rs src\ps_functions.rs
 
-D:\personal\projects\NanarC2\Nanar-Client\target\release\deps\nanar_client-e5c3293ef9a493c9.d: src\lib.rs src\fs_functions.rs src\fs_functions\fs_helper.rs src\connection_handler.rs src\ps_mgmt.rs
+D:\personal\projects\NanarC2\Nanar-Client\target\release\deps\nanar_client-e5c3293ef9a493c9.d: src\lib.rs src\fs_functions.rs src\fs_functions\fs_helper.rs src\connection_handler.rs src\ps_functions.rs
 
 src\lib.rs:
 src\fs_functions.rs:
 src\fs_functions\fs_helper.rs:
 src\connection_handler.rs:
-src\ps_mgmt.rs:
+src\ps_functions.rs:
