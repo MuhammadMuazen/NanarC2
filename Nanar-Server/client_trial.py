2419859ebe49e9a073144c47e05d5021f5eafcea
YazanAljeddawi <yazanaljeddawi@gmail.com>
2025-05-13 09:19:48 +0300

    walkthrough

--- Nanar-Server/client_trial.py
+++ Nanar-Server/client_trial.py
@@ -2,7 +2,7 @@ import socket
 import os
 import subprocess
 
-s = socket.socket(socket.AFINET,socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = '127.0.0.1'
 port = 9999
 
