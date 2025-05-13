9edc283c374f0f4467ce22e84d405fe0ee488000
Muhammad Muazen <muhammadmuazen1@gmail.com>
2025-05-13 01:30:32 -1200

    fixing stuff 4

--- Nanar-Client/target/debug/deps/windows_implement-3fe83e1d90022348.d
+++
@@ -1,6 +0,0 @@
-D:\personal\projects\NanarC2\Nanar-Client\target\debug\deps\windows_implement-3fe83e1d90022348.dll: C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-implement-0.60.0\src\lib.rs C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-implement-0.60.0\src\gen.rs
-
-D:\personal\projects\NanarC2\Nanar-Client\target\debug\deps\windows_implement-3fe83e1d90022348.d: C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-implement-0.60.0\src\lib.rs C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-implement-0.60.0\src\gen.rs
-
-C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-implement-0.60.0\src\lib.rs:
-C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-implement-0.60.0\src\gen.rs:
--- Nanar-Client/target/release/deps/windows_collections-68ab5ae29da746b7.d
+++
@@ -1,9 +0,0 @@
-D:\personal\projects\NanarC2\Nanar-Client\target\release\deps\libwindows_collections-68ab5ae29da746b7.rmeta: C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\lib.rs C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\bindings.rs C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\../readme.md
-
-D:\personal\projects\NanarC2\Nanar-Client\target\release\deps\libwindows_collections-68ab5ae29da746b7.rlib: C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\lib.rs C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\bindings.rs C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\../readme.md
-
-D:\personal\projects\NanarC2\Nanar-Client\target\release\deps\windows_collections-68ab5ae29da746b7.d: C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\lib.rs C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\bindings.rs C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\../readme.md
-
-C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\lib.rs:
-C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\bindings.rs:
-C:\Users\muhammad\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-collections-0.2.0\src\../readme.md:
