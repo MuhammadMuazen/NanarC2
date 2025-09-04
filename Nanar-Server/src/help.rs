c6fbe72271712b7534a947444b851795dc114f51
Muhammad Muazen <muhammadmuazen1@gmail.com>
2025-09-04 06:26:27 +0100

    finishing the help menu of the local server commands

--- Nanar-Server/src/help.rs
+++ Nanar-Server/src/help.rs
@@ -55,11 +55,11 @@ pub fn print_avaliable_local_commands() {
     println!(r#"    
 [i] These commands can be run inside the server command line interface:
 
-    [1] l, listen [options] --> Make the server listen in a way specifed by the options:
+    [1] l, listener [options] --> Make the server listen in the background in a way specifed by the options:
 
-        1) socket [ip] [port]
-        2) http [port]
-        3) https [port] [certificate-path]
+        1) socket [local-ip] [local-port]
+        2) http [local-ip] [local-port]
+        3) https [local-ip] [local-port] [certificate-path]
 
     [2] cc, create-client [options] --> Compiles a new client with new ID and communication password and add 
         them to the clients.json file (Requires cargo to be installed on your machine):
@@ -68,10 +68,33 @@ pub fn print_avaliable_local_commands() {
         2) -http [ip/domain] [port]         Specify the HTTP server the client will connect to.
         3) -https [ip/domain] [port]        Specify the HTTPS server the client will connect to.     
         4) -cd, --client-directory [path]   Specify the client directory path which will be compiled.
+        5) -sd, --self-delete               Add the self delete feature to the client.
 
-    [2] gcid, generate-client-id --> Generate a client id and add it to the clients.json file
+    [2] ls, list-sessions --> List all the clients sessions which are connected to the server.
 
-    [3] 
-      
+    [3] ll, list-listeners --> List all  the listeners which have no connections with clients yet.
+
+    [4] s, session [Session ID/Number in the list] [options] --> Select a session and do the option:
+        
+        1) -i, --interact                   Open the client session shell to interact with it.
+        2) -r, --remove                     Delete the client session and remove the ID from the clients.json file.
+        3) -d, --details                    Prints the session's connection details
+    
+    [5] config [option] --> Interact with the server modifications depending on the option:
+
+        1) -m,  --modify [config-key-name]   Modify the the value of the configuration specifed ({{ -m all }} can be used to run a dialog 
+                                             to change all the configurations values in nanar_config.json file).
+        2) -p,  --print                      Prints the contents of the nanar_config.json file and its path.
+        3) -sp, --set-path [path]            Point to a new configuration file by setting its path.
+    
+    [6] h, help --> Prints this help message.
+
+    [7] prc, print-remote-commands --> Prints all the avaliable commands that can be sent to the client.
+
+    [8] pc, print-clients --> Prints the contents of the json file that contains all the clients IDs.
+
+    [9] pcf, point-client-file [Path] --> Make the server point to another clients json file.
+
+    [8] exit --> Exit the server (Warning: This will terminate all the clients connections with the servers).
 "#);
 }
